01fd5509eccae98aa2f7844df74b413c8c900367
Charles CORCY <ccorcy@student.42.fr>
2019-05-30 20:13:52 +0200

    Initial Commit

---
+++ Iskatu's compagnon/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Iskatu's compagnon
+//
+//  Created by Charles Corcy on 30/05/2019.
+//  Copyright © 2019 Charles Corcy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Iskatu's compagnon/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Iskatu's compagnon
+//
+//  Created by Charles Corcy on 30/05/2019.
+//  Copyright © 2019 Charles Corcy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
